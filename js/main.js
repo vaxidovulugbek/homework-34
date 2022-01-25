0fc1ab3fbd63207348203a29743442a5e8bd3231
vakhidovulugbek03 <vulugbekk@gmail.com>
2022-01-25 08:58:22 +0500

    commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// let BTN = document.getElementById("modeBtn")
+
+// document.getElementById('modeBtn').addEventListener('click', function () {
+//   BTN.classList.toggle('toggl')
+// });
+
+
+modeBtn.addEventListener("click", function(){
+  modal1.classList.toggle("toggle-block")
+})
+
+// modeBtn2.addEventListener("click", function(){
+//   modal1.classList.toggle("toggle-block2")
+// })
+
+// modeBtn3.addEventListener("click", function(){
+//   modal1.classList.toggle("toggle-block3")
+// })
+
+// modeBtn4.addEventListener("click", function(){
+//   modal1.classList.toggle("toggle-block4")
+// })
+
+
